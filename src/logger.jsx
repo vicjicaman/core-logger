e7c06aa5191f41872c0514448140562384b23bf7
Victor Jimenez <vic.jicama@gmail.com>
2019-01-14 16:10:47 -0600

    Sync to iteration

--- src/logger.jsx
+++ src/logger.jsx
@@ -4,23 +4,23 @@ import path from 'path'
 
 export const Logger = ({path: logPath}) => {
 
-  if (!fs.existsSync(logPath)) {
-    fs.mkdirSync(logPath);
+  const transports = [];
+  if (logPath) {
+    if (!fs.existsSync(logPath)) {
+      fs.mkdirSync(logPath);
+    }
+
+    transports.push(new winston.transports.File({
+      filename: path.join(logPath, 'error.log'),
+      level: 'error'
+    }));
+
+    transports.push(new winston.transports.File({
+      filename: path.join(logPath, 'info.log')
+    }));
   }
 
-  const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-      new winston.transports.File({
-        filename: path.join(logPath, 'error.log'),
-        level: 'error'
-      }),
-      new winston.transports.File({
-        filename: path.join(logPath, 'info.log')
-      })
-    ]
-  });
+  const logger = winston.createLogger({level: 'info', format: winston.format.json(), transports});
 
   if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({level: "debug", format: winston.format.simple()}));
